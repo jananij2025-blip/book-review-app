c9ad400b9819e4f4d60f6f7e003c1dac88edd81f
jananij2025-blip <janani.j.2025@smu.edu.sg>
2026-06-02 13:42:42 +0800

    Connect app to MongoDB

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
- const express = require("express");
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("MongoDB Connected!");
+})
+.catch((err) => {
+    console.log(err);
+});
+
 app.get("/", (req, res) => {
-  res.send("Friend Photo App Running!");
+    res.render("home");
 });
 
 app.listen(3000, () => {
-  console.log("Server started on port 3000");
+    console.log("Server running on port 3000");
 });
